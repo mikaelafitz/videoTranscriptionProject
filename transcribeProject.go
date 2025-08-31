f94ac4e3ef64da9368973a11a89007346f2a5d06
Mikaela Fitz <mikaelafitz@ucsb.edu>
2025-08-30 18:22:28 -0700

    video file successfully adds to original bucket

--- transcribeProject.go
+++ transcribeProject.go
@@ -6,42 +6,34 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main(){
 	//ctx stops code when AWS takes more than 10 seconds to load
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
-
-	//check if filename is in the command line
-	if len(os.Args) < 2 {
-		fmt.Println("Please put file name in command line.")
-		return
-	}
-
-	//access the video file
-	videoFile := os.Args[1]
-
-	//check if file is valid
-	info, err := os.Stat(videoFile)
-	if os.IsNotExist(err) {
-		fmt.Println("file does not exist")
-		return
-	}
-	if info.IsDir() {
-		fmt.Println("this is a directory, not a file")
-		return
-	}
-	fmt.Println("file is valid!")
 
 	//log into aws
 	cfg := logIn(ctx)
 
+	//get original video file
+	filePath, err := getVideo()
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
 	//upload video file to s3 bucket
-	uploadFile(cfg, ctx)
+	err = uploadFile(cfg, ctx, filePath)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
+	fmt.Println("made it to the end")
 }
 
 func logIn(ctx context.Context) aws.Config{
@@ -64,6 +56,55 @@ func logIn(ctx context.Context) aws.Config{
 	return cfg
 }
 
-func uploadFile(cfg aws.Config, ctx context.Context) {
+func getVideo() (string, error) {
+	//check if filename is in the command line
+	if len(os.Args) < 2 {
+		return "", fmt.Errorf("Please put file name in command line")
+	}
 
+	//access the video file
+	filePath := os.Args[1]
+
+	//check if file is valid
+	info, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		return "", fmt.Errorf("file does not exist")
+	}
+	if info.IsDir() {
+		return "", fmt.Errorf("this is a directory, not a file")
+	}
+	fmt.Println("file is valid!")
+
+	return filePath, err
+}
+
+func uploadFile(cfg aws.Config, ctx context.Context, filePath string) error{
+	//create S3 client
+	client := s3.NewFromConfig(cfg)
+
+	//opens the file, returns error if cannot
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open video file: %w")
+	}
+	defer file.Close()
+
+	//fileName
+	fileName := filepath.Base(filePath)
+
+	//upload file into s3 bucket
+	_, err = client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String("transcription-job-original-files"),
+		Key: aws.String(fileName),
+		Body: file,
+	})
+
+	//check for errors
+	if err != nil {
+		return fmt.Errorf("failed to upload video into s3 bucket: %w", fileName, err)
+	}
+
+	//successfully uplaoded
+	fmt.Println("Successfully uploaded video into s3 bucket")
+	return nil
 }
